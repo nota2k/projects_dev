d073efdfb661dfa3bdcef8d8d740187ada3443d6
Nelly <nelly.babillon@gmail.com>
2025-01-07 10:41:37 +0100

    Client : add, all, one, delete OK

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Client } from './entities/client.entity';
+
 
 @Injectable()
 export class ClientsService {
+  constructor(
+    @InjectRepository(Client)
+    private clientsRepository: Repository<Client>,
+  ) {}
+
   create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+    const newClient = this.clientsRepository.create(createClientDto);
+    return this.clientsRepository.save(newClient);
   }
 
   findAll() {
-    return `This action returns all clients`;
+    const clients = this.clientsRepository.find();
+    return clients;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} client`;
+  findOne(id: number): Promise<Client> {
+    const client = this.clientsRepository.findOneBy({id});
+    return client;
   }
 
   update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
+    const client = this.clientsRepository.update(id, updateClientDto);
+    return client;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} client`;
+  async remove(id: number): Promise<void> {
+    await this.clientsRepository.delete(id);
   }
 }
